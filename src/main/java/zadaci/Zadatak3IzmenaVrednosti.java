a469920ff3a7968fdb8afebd0ca6a1121744187a
MihajloVitanovic <vitanovic.mihajlo@gmail.com>
2017-04-25 19:52:10 +0200

    Uradjen zadatak izmena vrednosti fixes #8

--- src/main/java/zadaci/Zadatak3IzmenaVrednosti.java
+++ src/main/java/zadaci/Zadatak3IzmenaVrednosti.java
@@ -1,18 +1,47 @@
 package zadaci;
 
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import model.Vagon;
+import model.Voz;
+
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 /**
  * Created by androiddevelopment on 25.4.17..
  */
 public class Zadatak3IzmenaVrednosti {
+    static Dao<Voz,Integer> vozDao;
+    static Dao<Vagon,Integer> vagonDao;
+
     public static void main(String[] args) {
         ConnectionSource connectionSource = null;
         try {
             connectionSource = new JdbcConnectionSource("jdbc:sqlite:vozVagon.db");
+            //Instanciranje Dao objekata
+            vozDao = DaoManager.createDao(connectionSource, Voz.class);
+            vagonDao = DaoManager.createDao(connectionSource, Vagon.class);
+
+            //Prikaz svih vrednosti tabela Vagon
+            List<Vagon> vagon = vagonDao.queryForAll();
+            for (Vagon vgn:vagon)
+                System.out.println("Vagon = " + vgn);
+
+            //Menjamo atribut opis za objekat Vagon
+            List<Vagon> pronadjenVagon = vagonDao.queryForEq(Vagon.POLJE_OPIS, "Restoran");
+            Vagon vagonZaIzmenu = pronadjenVagon.get(0);
+            vagonZaIzmenu.setOpis("Za sedenje");
+            vagonDao.update(vagonZaIzmenu);
+
+            //Prikaz svih vrednosti tabela Vagon
+            vagon = vagonDao.queryForAll();
+            for (Vagon vgn:vagon)
+                System.out.println("Vagon = " + vgn);
+
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
